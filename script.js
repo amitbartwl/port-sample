921a3ee3f61993d81526bfad15645bd32e614810
Amit Bartwal <167638650+amitbartwl@users.noreply.github.com>
2024-11-20 21:13:24 +0530

    Add files via upload
    complete bhaiya g

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+// Initialize tilt effect
+VanillaTilt.init(document.querySelectorAll("[data-tilt]"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.5
+});
